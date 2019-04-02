8e49eb29936046200ad82bf5df70a8543c0c5c50
yahyaabualhaj <yahya.abualhaj@gmail.com>
2019-04-02 17:21:33 +0300

    second & thered day

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    user_id = models.IntegerField()
+    listing = models.IntegerField()
+    listing_id = models.IntegerField()
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    phone = models.CharField(max_length=128)
+    message = models.TextField()
+    contact_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Listing
 
-# Create your views here.
+
+def index(request):
+    listings_list = Listing.objects.order_by('-list_date')
+    paginator = Paginator(listings_list, 3)
+
+    page = request.GET.get('page')
+    listings = paginator.get_page(page)
+    context = {'listings': listings}
+
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    context = {'listing': listing}
+    return render(request, 'listings/listing.html', context)
+
+
+def search(request):
+    return render(request, 'listings/search.html')
+
+
