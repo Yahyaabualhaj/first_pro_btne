f93f83d6395468f7b4ea15a4708caf06b2a15517
yahyaabualhaj <yahya.abualhaj@gmail.com>
2019-04-04 17:27:29 +0300

    4th day

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import Listing
+from .models import Listing, Realtor
+from listings.chioces import (price_choices,
+                              bedroom_choices,
+                              state_choices)
 
 
 def index(request):
@@ -16,11 +19,60 @@ def index(request):
 
 def listing(request, listing_id):
     listing = Listing.objects.get(id=listing_id)
-    context = {'listing': listing}
+    # realtor = Realtor.objects.get(id=listing.realtor)
+
+    context = {'listing': listing, }
+
     return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    #  QuerySet rules.
 
+    #  Rules 1 : Sorting
+    queryset_list = Listing.objects.order_by('-list_date')
+    # print(Listing.objects.values())
+    # print(Listing.objects.values_list('address', flat=True))
+    # print(Listing.objects.values_list('address', flat=False))
+
+    #  Rules 2 :filters == keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list=queryset_list.\
+                filter(description__icontains=keywords)
+
+    #  Rules 3 :filters == City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list. \
+                filter(description__iexact=city)
+
+    #  Rules 4 :filters == state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list. \
+                filter(description__exact=state)
+
+    #  Rules 4 :filters == bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list. \
+                filter(description__lte=bedrooms)
+
+    #  Rules 4 :filters == price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list. \
+                filter(description__lte=price)
+
+    return render(request, 'listings/search.html', {'state_choices': state_choices,
+                                                    'bedroom_choices': bedroom_choices,
+                                                    'price_choices': price_choices,
+                                                    'listings': queryset_list,
+                                                    'values': request.GET})
 
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from listings.models import Listing
-from  realtors.models import Realtor
+from realtors.models import Realtor
+from listings.chioces import (price_choices,
+                              bedroom_choices,
+                              state_choices)
 
 
 def index(request):
 
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
-    return render(request, 'pages/index.html', {'listings': listings})
+    return render(request, 'pages/index.html', {'listings': listings,
+                                                'state_choices': state_choices,
+                                                'bedroom_choices': bedroom_choices,
+                                                'price_choices': price_choices
+                                                })
 
 
 def about(request):
@@ -17,3 +24,5 @@ def about(request):
 
     return render(request, 'pages/about.html', {'realtors': realtors,
                                                 'mvp_realtors': mvp_realtors})
+
+
